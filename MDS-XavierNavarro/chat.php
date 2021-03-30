6e98b0b108eca2e17965830d034c9cd3a0e8efc0
xavi01 <xavier.navarro.mas@gmail.com>
2021-03-30 21:52:29 +0200

    chat

--- MDS-XavierNavarro/chat.php
+++ MDS-XavierNavarro/chat.php
@@ -26,6 +26,22 @@
     text-align: center;
 }
 
+#fotoperfilchat{
+    border-radius: 50px;
+}
+
+#fotoperfilgran{  /* boto a sobre la foto */
+    position: absolute;
+    width: 100;
+    height: 100;
+    background: transparent;
+    border: 0;
+    color: transparent;
+}
+
+
+
+
 
 
 
@@ -43,6 +59,13 @@
         if(isset($_REQUEST["escogerchat"])){ //BOTON PARA ESCOGER CHAT
             $_SESSION["chat"] = $_REQUEST["escogerchat"];          
         }
+
+        if(isset($_REQUEST["fotoperfilgran"])){ //BOTON PARA VER EL USUARIO
+            $_SESSION["usuario_elegido"] = $_REQUEST["fotoperfilgran"];         
+            header('Location: usuario.php');
+        }
+
+
 
         if(isset($_REQUEST["enviar"])){ //BOTON PARA ENVIAR MENSAJE
             $chatcon = $_SESSION["chat"];
@@ -116,26 +139,37 @@
            echo "<label for=''>Selecciona un chat para poder hablar.</label>";
         }else{
             $chat= $_SESSION["chat"];
-            echo "Este es el chat con " . $chat ."<br><br>";
-
+            
             $mysql = new mysqli ("localhost","root","","electroland");
 
             if($mysql->connect_error){
               die("Conexio fallida");
             }  
 
-            $consulta= "SELECT * FROM chat WHERE (id_de = '$usuario_activo' AND id_para = '$chat')  OR  (id_para = '$usuario_activo' AND id_de = '$chat')";
+            $consulta1= "SELECT fotoperfil FROM usuarios WHERE n_usuario='$chat'";
+            $resultatstaula1= $mysql->query($consulta1);
+
+            while($fila1 = $resultatstaula1->fetch_array()){
+              echo "<input type='submit' name='fotoperfilgran' value='" . $chat . "' height='100' width='100' id='fotoperfilgran'>"; 
+              echo "<img src='data:image/jpeg; base64," . base64_encode($fila1["fotoperfil"]) . "' id='fotoperfilchat' height='100' width='100'><br><br>";
+            }
+
+
+            echo "Este es el chat con " . $chat ."<br><br>";
+
+            $consulta= "SELECT c.mensaje, c.fecha, u.fotoperfil , c.id_de FROM chat c INNER JOIN usuarios u ON c.id_de=u.n_usuario WHERE (id_de = '$usuario_activo' AND id_para = '$chat')  OR  (id_para = '$usuario_activo' AND id_de = '$chat')";
             $resultatstaula= $mysql->query($consulta);
         
             while($fila = $resultatstaula->fetch_array()){
-        
-              echo $fila['id_de'] .": " . $fila['mensaje'] . ", " . $fila['fecha'] . "<br><br>";
+
+              echo "<img src='data:image/jpeg; base64," . base64_encode($fila["fotoperfil"]) . "' id='fotoperfilchat' height='40' width='40'>";
+              echo $fila['id_de'] .": " . $fila['mensaje'] . " - " . $fila['fecha'] . "<br><br>";         
               
             }
 
             $mysql->close();
             
-            echo"<input type='text' name='textoenviar' id=''>";
+            echo"<br><br><input type='text' name='textoenviar' id=''>";
             echo "<input type='submit' name='enviar' value='ENVIAR' id=''>";
         }
 
